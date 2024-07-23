349cc33fbb56cd7a7b73f8175232673e1842ffa6
chikage <lucas.benjebara9@gmail.com>
2024-07-23 03:44:37 +0200

    sfds

--- src/app.py
+++ src/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify
 import subprocess
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/run-script', methods=['POST'])
 def run_script():
     try:
-        result = subprocess.run(['python3', './teste.py'], capture_output=True, text=True, check=True)
+        result = subprocess.run(['python', './teste.py'], capture_output=True, text=True, check=True)
         return jsonify({'output': result.stdout})
     except subprocess.CalledProcessError as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Remplacez app.run() par serve()
+    serve(app, host='0.0.0.0', port=5000)
